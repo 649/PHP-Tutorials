f27415d498410755de1eaad2ec63db0de8c51b28
649 <649@users.noreply.github.com>
2018-01-22 13:01:42 -0800

    init

---
+++ GET_Welcome.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+Welcome <?php echo $_GET["name"]; ?>.<br />
+You are <?php echo $_GET["age"]; ?> years old!
+
+<!--- Data from GET Request is sent here and processed --->
+
+</body>
+</html>
